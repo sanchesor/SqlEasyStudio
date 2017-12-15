011861d7e47a14585d31459a960f377f64224931
Paweł Sawicki <pawel.sawicki@assecobs.pl>
2017-12-15 09:13:36 +0100

    resource clean

--- SqlEasyStudio/Properties/Resources.Designer.cs
+++ SqlEasyStudio/Properties/Resources.Designer.cs
@@ -69,15 +69,5 @@ namespace SqlEasyStudio.Properties {
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
-        
-        /// <summary>
-        ///   Looks up a localized resource of type System.Drawing.Bitmap.
-        /// </summary>
-        internal static System.Drawing.Bitmap ses_bmp {
-            get {
-                object obj = ResourceManager.GetObject("ses_bmp", resourceCulture);
-                return ((System.Drawing.Bitmap)(obj));
-            }
-        }
     }
 }
