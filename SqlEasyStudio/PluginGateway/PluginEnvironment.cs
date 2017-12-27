34538071c51ad597c79121f3a24fc348168ac902
sanchesor <sanchesor@gmail.com>
2017-12-27 17:53:24 +0100

    environment

---
+++ SqlEasyStudio/PluginGateway/PluginEnvironment.cs
@@ -0,0 +1,23 @@
+﻿using Kbg.NppPluginNET.PluginInfrastructure;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SqlEasyStudio.PluginGateway
+{
+    public static class PluginEnvironment
+    {
+        public static string ConfigDir
+        {
+            get
+            {
+                StringBuilder sbIniFilePath = new StringBuilder(Win32.MAX_PATH);
+                Win32.SendMessage(PluginBase.nppData._nppHandle, (uint)NppMsg.NPPM_GETPLUGINSCONFIGDIR, Win32.MAX_PATH, sbIniFilePath);
+                return sbIniFilePath.ToString();
+            }
+        }
+    }
+}
+    
